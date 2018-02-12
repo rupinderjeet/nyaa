9415ff74459c2772b82e91132883683565bc8483
Rupinderjeet <rupinderjeet47@gmail.com>
2018-02-13 01:43:25 +0530

    adds `/categories` endpoint with sample

--- nyaa/api/nyaa_api.py
+++ nyaa/api/nyaa_api.py
@@ -45,3 +45,78 @@ def safe_print(message):
 def error(message, status_code=400):
     safe_print('[{0}] {1}'.format(message, status_code))
     return flask.jsonify({'errors': [message]}), status_code
+
+###################### API ROUTES ########################
+
+##############################################
+#              Categories
+###############################################
+
+@api_v3_blueprint.route('/categories', methods=['GET'])
+# @basic_auth_user
+# @api_require_user
+def v3_api_categories():
+
+    """
+    Used to fetch categories
+
+    - no pagination required
+    - no params required
+
+    :return: a collection of categories as JSON
+
+    see sample_categories.json
+    """
+
+    categories_result = models.MainCategory.query
+
+    if not categories_result:
+        return error('Categories not found.')
+
+    # Method 1
+    categories = []
+    for category in categories_result:
+
+        sub_categories = []
+
+        if category and category.sub_categories:
+            for sub_category in category.sub_categories:
+
+                if sub_category:
+                    sub_categories.append(
+                        {
+                            'id': sub_category.id,
+                            'name': sub_category.name,
+                            'id_as_string': sub_category.id_as_string
+                        }
+                    )
+
+        main_category_metadata = {
+            'id': category.id,
+            'name': category.name,
+            'id_as_string': category.id_as_string,
+            'sub_categories': sub_categories
+        }
+
+        categories.append(main_category_metadata)
+
+    # Method 2
+
+    # categories = [
+    #     {
+    #         'id': category.id,
+    #         'name': category.name,
+    #         'id_as_string': category.id_as_string,
+    #         'sub_categories': [
+    #             {
+    #                 'id': sub_category.id,
+    #                 'id_as_string': sub_category.id_as_string,
+    #                 'name': sub_category.name
+    #             } for sub_category in category.sub_categories
+    #             # TODO: how to check if category.sub_categories is None or not?
+    #         ]
+    #     } for category in categories_result
+    #     # TODO: how to check if category is None or not?
+    # ]
+
+    return flask.jsonify(categories), 200
